9bb0ef75c805f03679de7cb078a892f1d0843459
Malmee Weerasinghe <malmee-work@gmailcom>
2023-11-23 12:19:26 +0000

    token url request method added

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -12,6 +12,32 @@ const clerkProvider = {
   },
   token: {
     url: `${process.env.CLERK_TOKEN_URL}`,
+    async request(data:any){
+  const code = data.params.code;
+  const tokenParams = new URLSearchParams({
+    grant_type: "authorization_code",
+    client_id: `${process.env.CLERK_CLIENT_ID}`,
+    client_secret: `${process.env.CLERK_SECRET_KEY}`,
+    redirect_uri: `${process.env.NEXTAUTH_URL}${process.env.REDIRECT_PATH}`,
+    code: `${code}`,
+  });
+  
+  const res = await fetch(
+    `${process.env.CLERK_TOKEN_URL}?${tokenParams.toString()}`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        code,
+      }),
+    }
+  );
+
+  const token = await res.json();
+  return {tokens: token};
+    }
   },
   userinfo: {
     url: `${process.env.CLERK_USER_INFO_URL}`,
@@ -24,7 +50,7 @@ const authOptions = {
     providers: [
       clerkProvider
     ],
-    debug: true,
+    // debug: true,
 } as unknown as NextAuthOptions
 
 const handler = NextAuth(authOptions);
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,12 @@
+import { SessionProvider } from "next-auth/react";
+
 export default function Dashboard() {
+  
   return (
+    <SessionProvider>
     <div >
      Hello dashboard
     </div>
+    </SessionProvider>
   );
 }
