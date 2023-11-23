15005dafb0dc037732fb6623a38f3357163be808
Malmee Weerasinghe <malmee-work@gmailcom>
2023-11-23 16:48:57 +0000

    update bc id in metadata in clerk

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,5 @@
-import NextAuth, { NextAuthOptions } from "next-auth"
+import clerkClient from "@clerk/clerk-sdk-node";
+import NextAuth, { NextAuthOptions } from "next-auth";
 
 const clerkProvider = {
   id: "clerkprovider",
@@ -10,92 +11,96 @@ const clerkProvider = {
     url: `${process.env.CLERK_AUTH_URL}`,
     params: { scope: "email profile public_metadata" },
   },
-  profile(profile:any) {
+  profile(profile: any) {
     return {
       id: profile.user_id,
       name: profile.name,
       email: profile.email,
       image: profile.picture,
-    }
+    };
   },
   token: {
     url: `${process.env.CLERK_TOKEN_URL}`,
-    async request(data:any){
-  const code = data.params.code;
-  const tokenParams = new URLSearchParams({
-    grant_type: "authorization_code",
-    client_id: `${process.env.CLERK_CLIENT_ID}`,
-    client_secret: `${process.env.CLERK_SECRET_KEY}`,
-    redirect_uri: `${process.env.NEXTAUTH_URL}${process.env.REDIRECT_PATH}`,
-    code: `${code}`,
-  });
-  
-  const res = await fetch(
-    `${process.env.CLERK_TOKEN_URL}?${tokenParams.toString()}`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        code,
-      }),
-    }
-  );
+    async request(data: any) {
+      const code = data.params.code;
+      const tokenParams = new URLSearchParams({
+        grant_type: "authorization_code",
+        client_id: `${process.env.CLERK_CLIENT_ID}`,
+        client_secret: `${process.env.CLERK_SECRET_KEY_GENERATED}`,
+        redirect_uri: `${process.env.NEXTAUTH_URL}${process.env.REDIRECT_PATH}`,
+        code: `${code}`,
+      });
 
-  const token = await res.json();
-  console.log('token', token)
-  return {tokens: token};
-    }
+      const res = await fetch(
+        `${process.env.CLERK_TOKEN_URL}?${tokenParams.toString()}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            code,
+          }),
+        }
+      );
+
+      const token = await res.json();
+      return { tokens: token };
+    },
   },
   userinfo: {
     url: `${process.env.CLERK_USER_INFO_URL}`,
   },
   clientId: `${process.env.CLERK_CLIENT_ID}`,
-  clientSecret: `${process.env.CLERK_SECRET_KEY}`,
+  clientSecret: `${process.env.CLERK_SECRET_KEY_GENERATED}`,
 };
 const authOptions = {
-    // Configure one or more authentication providers
-    providers: [
-      clerkProvider
-    ],
-    callbacks: {
-        async session({ session, user, token }) {
-            console.log('session callback', session, user, token)
-          return session
-        },
-        async jwt({ token, user, account, profile, isNewUser }) {
-          console.log('jwt callback', token, user, account, profile, isNewUser)
-            if(profile){
-                    if(!profile.public_metadata?.bigcommerceId) {
-                      console.log('no bc id')
+  // Configure one or more authentication providers
+  providers: [clerkProvider],
+  callbacks: {
+    async session({ session, token }) {
+      session.user.bcId = token.bcId
+      return session
+    },
+    async jwt({ token,profile }) {
+      if (profile) {
+        if (!profile.public_metadata?.bigcommerceId) {
 
-                    const accounts = await fetch('https://api.bigcommerce.com/stores/3q09xykre/v3/customers',  {
-                        method: "POST",
-                        headers: {
-                          "Content-Type": "application/json",
-                          "X-Auth-Token": "2ty9zt9g1h0xrx42444okydk0ne8ng9"
-                        },
-                        body: JSON.stringify([{
-                          email: `${profile.email}6`,
-                          first_name: profile.given_name,
-                          last_name: profile.family_name
-                      }]),
-                      } )
-                      const bcaccounts = await accounts.json()
-                      console.log('bcaccounts', bcaccounts);
-                      const bcId = bcaccounts.data[0].id;
-                      console.log('bcId', bcId);
-                }
+          const accounts = await fetch(
+            `https://api.bigcommerce.com/stores/${process.env.BIGCOMMERCE_STORE}/v3/customers`,
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+                "X-Auth-Token": `${process.env.BIGCOMMERCE_KEY}`,
+              },
+              body: JSON.stringify([
+                {
+                  email: `${profile.email}10`,
+                  first_name: profile.given_name,
+                  last_name: profile.family_name,
+                },
+              ]),
             }
-            
-          return token
+          );
+          const bcaccounts = await accounts.json();
+          const bcId = bcaccounts.data[0].id;
+          await clerkClient.users.updateUserMetadata(
+            profile.user_id,
+            {
+              publicMetadata: {
+                bigcommerceId: bcId,
+              },
+            }
+          );
+          token.bcId= bcId;
         }
-      },
-    debug: true,
-} as unknown as NextAuthOptions
+      }
+      return token;
+    },
+  },
+} as unknown as NextAuthOptions;
 
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST }
-
+export { handler as GET, handler as POST };
--- components/profile.tsx
+++ components/profile.tsx
@@ -5,9 +5,10 @@ import { SignIn, SignOut } from "./auth-components"
 export function Profile({
     provider,
   }: { provider?: string }) {
-    const { data: session, status } = useSession()
+    const { data: session, status } = useSession();
     if (status === "authenticated") {
-        return <div><p>Signed in as {session?.user?.email}</p><SignOut></SignOut></div>
+        return <div><p>Signed in as {session?.user?.email}</p>
+        <p>My bc account id {session?.user?.bcId}</p><SignOut></SignOut></div>
       }
     
       return <SignIn></SignIn>
