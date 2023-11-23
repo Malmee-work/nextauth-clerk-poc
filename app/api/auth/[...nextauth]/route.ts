33ab62e1ff82239b250ddbefc2ef355c230c266a
Malmee Weerasinghe <malmee-work@gmailcom>
2023-11-23 16:20:24 +0000

    create bc id

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -59,6 +59,39 @@ const authOptions = {
     providers: [
       clerkProvider
     ],
+    callbacks: {
+        async session({ session, user, token }) {
+            console.log('session callback', session, user, token)
+          return session
+        },
+        async jwt({ token, user, account, profile, isNewUser }) {
+          console.log('jwt callback', token, user, account, profile, isNewUser)
+            if(profile){
+                    if(!profile.public_metadata?.bigcommerceId) {
+                      console.log('no bc id')
+
+                    const accounts = await fetch('https://api.bigcommerce.com/stores/3q09xykre/v3/customers',  {
+                        method: "POST",
+                        headers: {
+                          "Content-Type": "application/json",
+                          "X-Auth-Token": "2ty9zt9g1h0xrx42444okydk0ne8ng9"
+                        },
+                        body: JSON.stringify([{
+                          email: `${profile.email}6`,
+                          first_name: profile.given_name,
+                          last_name: profile.family_name
+                      }]),
+                      } )
+                      const bcaccounts = await accounts.json()
+                      console.log('bcaccounts', bcaccounts);
+                      const bcId = bcaccounts.data[0].id;
+                      console.log('bcId', bcId);
+                }
+            }
+            
+          return token
+        }
+      },
     debug: true,
 } as unknown as NextAuthOptions
 
